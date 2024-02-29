d2d55d55166b19316999857e876ccd2d056a5154
Junhyeong Park <pljh0906@yonsei.ac.kr>
2024-02-29 13:14:35 +0900

    Deleting docz

--- my-app/.docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/c5cb8cf80172e723a395c0fc535fe7d4.js
+++
@@ -1,9 +0,0 @@
-import { Playground, Props } from 'docz';
-import UploadFileButton from "../../../../../src/UploadFileButton";
-import * as React from 'react';
-export default {
-  Playground,
-  Props,
-  UploadFileButton,
-  React
-};
