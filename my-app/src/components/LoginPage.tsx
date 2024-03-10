7f4a4281547eec09daf0560d0bcaf1259da07e58
Junhyeong Park <pljh0906@yonsei.ac.kr>
2024-03-10 19:46:25 +0900

    feat: login/signup page component ui develop

--- my-app/src/components/LoginPage.tsx
+++ my-app/src/components/LoginPage.tsx
@@ -2,17 +2,21 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import { useHighContrast } from "./HighContrastMode";
 
 const LoginPage: React.FC = () => {
   const [loginId, setLoginId] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [error, setError] = useState<string>("");
+  const { isHighContrast } = useHighContrast();
+
   const navigate = useNavigate();
   const { setLoggedIn } = useAuth();
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post("/login", {
+      const apiUrl = process.env.REACT_APP_API_URL;
+      const response = await axios.post(`${apiUrl}/login`, {
         loginId,
         password,
       });
@@ -39,21 +43,34 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={loginId}
-        onChange={(e) => setLoginId(e.target.value)}
-        placeholder="Login ID"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <button onClick={handleLogin}>Login</button>
-      {error && <p>{error}</p>}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-stone-200">
+      <div className="p-8">
+        <input
+          className="w-full p-2 mb-4 border rounded"
+          type="text"
+          value={loginId}
+          onChange={(e) => setLoginId(e.target.value)}
+          placeholder="Login ID"
+        />
+        <input
+          className="w-full p-2 mb-4 border rounded"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button
+          className={`${
+            isHighContrast
+              ? "bg-yellow-300 hover:bg-yellow-600"
+              : "bg-[#FF6A3F] hover:bg-[#E6552F]"
+          } w-full p-2`}
+          onClick={handleLogin}
+        >
+          Login
+        </button>
+        {error && <p>{error}</p>}
+      </div>
     </div>
   );
 };
